145e99f676e35c2a74bcc35278e759223ce657ce
haniagajek <playerhania1@gmail.com>
2023-06-11 21:15:38 +0200

    added backdrop

--- my-app/src/Blog.js
+++ my-app/src/Blog.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { createPortal } from "react-dom";
 
 export default function Blog({ blog, handleDelete, setEditingBlog }) {
   const [isMenuShown, setIsMenuShown] = useState(false);
@@ -13,31 +14,33 @@ export default function Blog({ blog, handleDelete, setEditingBlog }) {
 
   return (
     <div className={`blog-preview ${blog.color}`} key={blog.id}>
-      {/* <div onClick={() => handleDelete(blog.id)} className="Delete">
-    {" "}
-    =
-  </div> */}
-
       <div className="Edition">
         <div onClick={toggleMenu} className="edition-icon">
           =
         </div>
         {isMenuShown && (
-          <div className="menu">
-            <div onClick={showEdit} className="menu-item">
-              Edit
+          <>
+            <div className="menu">
+              <div onClick={showEdit} className="menu-item">
+                Edit
+              </div>
+              <div onClick={() => handleDelete(blog.id)} className="menu-item ">
+                Delete
+              </div>
             </div>
-            <div onClick={() => handleDelete(blog.id)} className="menu-item ">
-              Delete
-            </div>
-          </div>
+            {createPortal(
+              <div
+                onClick={() => setIsMenuShown(false)}
+                className="backdrop"
+              />,
+              document.body
+            )}
+          </>
         )}
       </div>
-
       <h2 className="blogTitle">{blog.title}</h2>
       <p className="Data">{blog.data}</p>
       <p className="blogBody"> {blog.body}</p>
-
       <div className="Home"></div>
     </div>
   );
